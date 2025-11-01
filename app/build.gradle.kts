b601448f545354a4fef8424193bc8675cd315ab4
ADMIN <shalenmj@gmail.com>
2025-11-01 17:59:29 +0530

    fix : version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.example.movieflix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
 
