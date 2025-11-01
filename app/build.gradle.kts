b200767e013f652234f2482262a286d76724866a
ADMIN <shalenmj@gmail.com>
2025-11-01 19:36:27 +0530

    fix : updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,13 @@ android {
 
     }
 
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+
     buildTypes {
         release {
             buildConfigField("String", "MOVIE_API_KEY", movieApiKey)
