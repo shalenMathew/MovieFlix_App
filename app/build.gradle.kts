a8e571a14adcae10c61349e8e574b79f2c9dfe07
ADMIN <shalenmj@gmail.com>
2026-08-09 23:20:49 +0530

    1.5 : version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.shalenmathew.movieflix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
 
